d2139fc5a3f2ad5d9704e9d7a589afa1e7f19047
BrianRonin <01100010v@gmail.com>
2023-01-27 08:37:46 -0300

    up

---
+++ packages/ui/src/styles/colors.stories.tsx
@@ -0,0 +1,14 @@
+import type {
+  Meta,
+  StoryObj,
+} from '@storybook/react'
+
+const meta = {
+  title: 'Example/Button/C0Button',
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Base = {}
